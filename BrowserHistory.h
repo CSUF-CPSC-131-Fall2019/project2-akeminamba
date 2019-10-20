ad751dd94d91bf9f80b51932da09416984696164
Your Name <you@example.com>
2019-10-20 08:58:07 -0700

    working on project 2 still

--- BrowserHistory.h
+++ BrowserHistory.h
@@ -7,6 +7,25 @@
 
 using std::string;
 
+template <typename T>
+struct Node {
+  T data;
+  Node<T>* next;
+  Node <T>* prev;
+
+  Node() = delete;
+  Node (const T & element) : data(element), next(nullprt), prev(nullptr) {}
+};
+
+class WebInfo {
+public:
+  string uniform_rl;
+  size_t page_size;
+
+  WebInfo(string url, size_t pgsize) : uniform_rl(url), page_size(pgsize) {}
+  WebInfo() {}
+};
+
 class BrowserHistory {
 public:
     BrowserHistory();
@@ -23,4 +42,64 @@ public:
 
 private:
   // TO BE COMPLETED
+  Node<WebInfor>* head;
+  Node<WebInfo>* tail;
+  size_t length_;
+  size_t accumilator_ = 0;
+  Node<WebInfo>* currNode;
 };
+
+BrowserHistory :: BrowserHistory() {
+  head = new Node<WebInfo>(WebInfo());
+  tail = new Node<WebInfor>(WebInfo());
+
+  head->next = tail;
+  tail->prev = head;
+}
+
+void BrowserHistory :: visitSite(string url, size_t filesize) {
+  WebInfo New;
+  New.uniform_rl = url;
+  New.page_size = filesize;
+  if(accumilator_ > 0) {
+    while(accumilator_ > 0 && accumilator_ != 0) {
+      remove(tail->prev);
+      accumilator_--;
+    }
+  }
+  Node<WebInfo>* newNode = new Node<WebInfo>(New);
+
+  newNode->prev = tail->prev;
+  newNode->next = tail;
+  tail->prev->next = newNode;
+  tail->prev = newNode;
+
+  currNode = newNode;
+}
+
+void BrowserHistory :: backButton() {
+  if(currNode->prev == head) {
+    std::cout << "No Previous Page/n";
+  }
+  else {
+    currNode = currNode->prev;
+    accumilator++;
+  }
+}
+
+void BrowserHistory :: forwardButton() {
+  if(currNode->next == tail) {
+    std::cout << "No Such Page'n";
+  }
+  else {
+    currNode = currNode->next;
+    accumilator_--;
+  }
+}
+
+void BrowserHistory :: readFile(string filename) {
+  string var0, var1, varTrash;
+  int var2;
+  std::ifstream meesafile(filename);
+  
+}
